f59ca2e1099f42a530a42ffa79479b66bc0e9ddb
Victor Chomette <victorchomette@hotmail.fr>
2021-11-11 16:55:52 +0100

    back done, front in progress

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # GET "lists" -- index list
+  # GET "lists/42" -- show list
+  # GET "lists/new" -- new list
+  # POST "lists" -- create list
+  resources :lists, only: [:index, :show, :new, :create] do
+    resources :bookmarks, only: [:new, :create]
+  end
+  resources :bookmarks, only: [:destroy]
 end
